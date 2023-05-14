8199047b41058fa9277529c3382967e4411b2f62
Eric Knight <datendrache@hotmail.com>
2023-05-14 16:05:58 -0600

    Add project files.

---
+++ BaseIndex.cs
@@ -0,0 +1,48 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+
+namespace FatumAnalytics
+{
+    public class BaseIndex
+    {
+        public string directory = "";
+        public IndexWriter indexer = null;
+        public Analyzer analyzer = null;
+        public Lucene.Net.Store.Directory indexdirectory = null;
+
+        public BaseIndex(string Directory)
+        {
+            directory = Directory;
+            
+            indexdirectory = FSDirectory.Open(new DirectoryInfo(Directory));
+            analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            try
+            {
+                indexer = new IndexWriter(indexdirectory, analyzer, true, IndexWriter.MaxFieldLength.LIMITED);
+            }
+            catch (Exception)
+            {
+                // cannot open database, probably because its already been opened for today
+                indexer = null;
+            }
+        }
+
+        public void Close()
+        {
+            if (indexer != null)
+            {
+                indexer.Dispose();
+                indexer = null;
+            }
+
+            if (analyzer!= null)
+            {
+                analyzer.Close();
+                analyzer.Dispose();
+                analyzer = null;
+            }
+        }
+    }
+}
---
+++ SearchModule.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+
+namespace FatumAnalytics
+{
+    public class SearchModule
+    {
+        public ArrayList activeIndexes = new ArrayList();
+
+        public void NewIndex(string directory, string filename)
+        {
+            
+            Lucene.Net.Store.Directory indexdirectory = FSDirectory.Open(new DirectoryInfo(directory + "\\" + filename));
+            Analyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            IndexWriter indexer = new IndexWriter(indexdirectory, analyzer, true, IndexWriter.MaxFieldLength.LIMITED);
+            activeIndexes.Add(indexer);
+        }
+
+        public void AdvanceIndexOnePeriod()
+        {
+
+        }
+
+        public void CloseIndex(IndexWriter indexer)
+        {
+            indexer.Commit();
+        }
+
+    }
+}
