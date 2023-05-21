72247d8dd03b209ba0ffd3183532151cb99269dd
Eric Knight <datendrache@hotmail.com>
2023-05-21 11:28:25 -0600

    Updates to 1.0.2

--- BaseIndex.cs
+++ BaseIndex.cs
@@ -1,11 +1,29 @@
-﻿using Lucene.Net.Analysis;
+﻿//   Fatum Analytics -- Performance Analytics for Fatum
+//
+//   Copyright (C) 2003-2023 Eric Knight
+//   This software is distributed under the GNU Public v3 License
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Index;
 using Lucene.Net.Store;
 
 namespace FatumAnalytics
 {
-    public class BaseIndex
+    public sealed class BaseIndex
     {
         public string directory = "";
         public IndexWriter indexer = null;
--- SearchModule.cs
+++ SearchModule.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections;
+﻿//   Fatum Analytics -- Performance Analytics for Fatum
+//
+//   Copyright (C) 2003-2023 Eric Knight
+//   This software is distributed under the GNU Public v3 License
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Index;
@@ -6,9 +23,9 @@ using Lucene.Net.Store;
 
 namespace FatumAnalytics
 {
-    public class SearchModule
+    public sealed class SearchModule
     {
-        public ArrayList activeIndexes = new ArrayList();
+        public List<IndexWriter> activeIndexes = new();
 
         public void NewIndex(string directory, string filename)
         {
@@ -28,6 +45,5 @@ namespace FatumAnalytics
         {
             indexer.Commit();
         }
-
     }
 }
